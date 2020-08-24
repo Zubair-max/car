ef08bf052fc00ad55fd5464374663ddd089ca52b
Zubair-max <66228164+Zubair-max@users.noreply.github.com>
2020-08-24 17:56:42 -0500

    Add files via upload

---
+++ p5.play-boilerplate-master (11)/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,23 @@
+var wall,car;
+var speed,weight
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+  speed=random(55,90)
+  weight=random(400,1500)
+}
+
+
+
+
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+  car=createSprite(50,200,50,50)
+wall=createSprite(1500,200,60,height/2)
+car.velocityX=speed;
+}
